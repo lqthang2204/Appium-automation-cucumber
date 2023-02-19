79df6bcbc61774c54c7ff5ce4a541429634476a5
Thang Le <quangthangle1402@gmail.com>
2023-02-19 17:02:06 +0700

    change name of class to run by maven
    updated

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue = {"StepDefinition"})
+public class RunnerTest extends AbstractTestNGCucumberTests {
+
+}
