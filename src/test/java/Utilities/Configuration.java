cf20a4ea6e364aef09ee9fd66de05485f2ba5d78
Thang Le <quangthangle1402@gmail.com>
2023-02-26 22:51:27 +0700

    update run by parallel in by testNG
    updated

--- src/test/java/Utilities/Configuration.java
+++ src/test/java/Utilities/Configuration.java
@@ -1,5 +1,7 @@
 package Utilities;
 
+import org.testng.annotations.Parameters;
+
 public class Configuration {
     public static String  AUTOMATION_NAME;
     public static String PLATFORM_NAME;
@@ -7,20 +9,35 @@ public class Configuration {
     public static String APP_PACKAGE;
     public static String APP_ACTIVITY;
     public static String PATH_SERVER;
-    public static void readConfig(){
-        Configuration.AUTOMATION_NAME =  System.getProperty("automation_name");
-        Configuration.PLATFORM_NAME = System.getProperty("platform_name");
-        Configuration.UD_ID = System.getProperty("udid");
-        Configuration.APP_PACKAGE = System.getProperty("appPackage");
-        Configuration.APP_ACTIVITY = System.getProperty("appActivity");
-        String port_number = System.getProperty("port_number");
-        System.out.println("portnumber============= "+ port_number);
-        Configuration.PATH_SERVER = "http://127.0.0.1:"+port_number+"/wd/hub";
 
+    public static void readConfig(String automation_name, String platform_name,String udid, String appPackage, String appActivity, String port_number){
+        if(System.getProperty("automation_name")==null){
+            readConfigFromXML(automation_name, platform_name,udid, appPackage, appActivity, port_number);
+        }else{
+            Configuration.AUTOMATION_NAME =  System.getProperty("automation_name");
+            Configuration.PLATFORM_NAME = System.getProperty("platform_name");
+            Configuration.UD_ID = System.getProperty("udid");
+            Configuration.APP_PACKAGE = System.getProperty("appPackage");
+            Configuration.APP_ACTIVITY = System.getProperty("appActivity");
+            String port = System.getProperty("port_number");
+            Configuration.PATH_SERVER = "http://127.0.0.1:"+port+"/wd/hub";
 //        checkNull(Configuration)
-        System.out.println("========udid============11 "+ Configuration.UD_ID);
-        System.out.println("System.getProperty(\"appPackage\")======================111 "+ System.getProperty("appPackage"));
-        System.out.println(" Configuration.PATH_SERVER 333 "+ Configuration.PATH_SERVER);
+            System.out.println("========udid============11 "+ Configuration.UD_ID);
+            System.out.println("System.getProperty(\"appPackage\")======================111 "+ System.getProperty("appPackage"));
+            System.out.println(" Configuration.PATH_SERVER 333 "+ Configuration.PATH_SERVER);
+        }
+
+    }
+
+
+    public static void readConfigFromXML(String automation_name, String platform_name,String udid, String appPackage, String appActivity, String port_number){
+        System.out.println("chay lan tu 222222222222222222222222222222");
+        Configuration.AUTOMATION_NAME =  automation_name;
+        Configuration.PLATFORM_NAME = platform_name;
+        Configuration.UD_ID = udid;
+        Configuration.APP_PACKAGE = appPackage;
+        Configuration.APP_ACTIVITY = appActivity;
+        Configuration.PATH_SERVER = "http://127.0.0.1:"+port_number+"/wd/hub";
     }
     public void checkNull(String value, String message){
         if(value==null){
--- src/test/resources/features/lazada.feature
+++ src/test/resources/features/lazada.feature
@@ -1,20 +1,20 @@
-@feature-mc-21946 @regression
+@feature-mc-21946 @lazada
 Feature: Login function
 
-  @mc-test
-    Scenario: Open App mobile
+  @mc-test-lazada
+    Scenario: Open App lazada
     Given I open application
     And I change the page spec to LoginPage
     And I click element option-language with text "Vietnam"
     And I change the page spec to HomePage
     And I wait for element confirm-option to be ENABLED
     And I click element confirm-option
-    And I change the page spec to IndexPage
-    And I wait for element search-field to be DISPLAYED
-    And I click element search-field
+#    And I change the page spec to IndexPage
+#    And I wait for element search-field to be DISPLAYED
+#    And I click element search-field
 #    And I click into element have text "KHÔNG, CẢM ƠN" to be DISPLAYED
-    And I wait for element search-input to be DISPLAYED
-    And I type "áo gió" into element search-input
-    And I wait for element search-button to be ENABLED
-    And I click element search-button
+#    And I wait for element search-input to be DISPLAYED
+#    And I type "áo gió" into element search-input
+#    And I wait for element search-button to be ENABLED
+#    And I click element search-button
 
